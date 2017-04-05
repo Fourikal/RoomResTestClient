30dba533253e9a9b3b384d0b1985b1690a6de5b3
andersnykaas <andersnykaas@gmail.com>
2017-04-05 08:07:06 +0200

    Testing client work

--- client.py
+++ client.py
@@ -1,4 +1,5 @@
 __author__ = 'Anders'
+import datetime
 import time
 import paho.mqtt.client as mqtt
 import json
@@ -11,7 +12,8 @@ def on_connect(client, userdata, flags, rc):
 def on_message(client, userdata, msg):
     data=json.loads(str(msg.payload)[2:-1])
     for i in data:
-        print(i['Name'])
+        #print(i['Name'], i['Id'])
+        print(i)
 
 
 client=mqtt.Client()
@@ -26,6 +28,13 @@ while True:
     print("Skriv inn commando")
     inn=input()
     if inn=='list':
-        data={'command': 'list'}
+        #data={'command': 'list'}
+        #verdi1=int(datetime.datetime(2017, 3, 31, 17, 00).timestamp())#debug
+        #verdi2=int(datetime.datetime(2017, 3, 31, 18, 00).timestamp())#debug
+        verdi1=1000#debug
+        verdi2=1300#debug
+        data={'command': 'list', 'building': 'Realfagsbygget', 'from' : verdi1 , 'to' : verdi2}#debug
+        #data={'command': 'list', 'building': 'Realfagsbygget'}#debug
+        print(data)
         client.publish('/hopp/ned', json.dumps(data))
         print("sendt")
